070059f82188bdaa869b1287ebe399bd7f4eb3cb
Arithorn <peet@gouws.com>
2024-02-27 12:16:52 +0000

    fix: add DB host

--- src/db.mjs
+++ src/db.mjs
@@ -5,6 +5,7 @@ import "dotenv/config";
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
+  port: DB_PORT,
   dialect: "postgres",
 });
 
